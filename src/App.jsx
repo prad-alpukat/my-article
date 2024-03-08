c7a58a9f876a2c9e48480524665aba3d8f178d5c
praditya aldi syahputra <pradityaaldi.s@gmail.com>
2024-03-08 13:08:48 +0700

    create routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// import pages
+import Home from "./pages/Home"
+import SinglePost from "./pages/SinglePost"
+import AdminLogin from "./pages/AdminLogin"
+import AdminDashboard from "./pages/AdminDashboard"
+import CreatePost from "./pages/CreatePost"
+import EditPost from "./pages/EditPost"
+
 export default function App() {
   return (
-    <div className="prose">
-      <h1>Berhasil Push Ke Github</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts/:id" element={<SinglePost />} />
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/home" element={<AdminDashboard />} />
+        <Route path="/admin/create-post" element={<CreatePost />} />
+        <Route path="/admin/edit-post/:id" element={<EditPost />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
