ea7380217e6b5d93aba698d42e1ace728b85bbb4
praditya aldi syahputra <pradityaaldi.s@gmail.com>
2024-03-08 17:41:05 +0700

    add footer compoennt

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer className='mt-auto text-white bg-slate-900'>
+            <div className="container py-4 text-center">
+                Create by <a className='font-bold text-underline' href="https://github.com/prad-alpukat">Prad-Alpukat</a>
+            </div>
+        </footer>
+    )
+}
--- src/layouts/ClientLayout.jsx
+++ src/layouts/ClientLayout.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function ClientLayout({ children }) {
     return (
-        <>
+        <div className='flex flex-col min-h-screen'>
             <Navbar />
 
             <main>
                 {children}
             </main>
 
-            <footer>Footer</footer>
-        </>
+            <Footer />
+        </div>
     )
 }
