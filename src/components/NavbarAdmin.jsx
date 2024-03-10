72b0a834b003ac9a816e97bea674d2c404c6fec8
praditya aldi syahputra <pradityaaldi.s@gmail.com>
2024-03-10 20:31:58 +0700

    add navbar admin

---
+++ src/components/NavbarAdmin.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function NavbarAdmin() {
+    return (
+        <div className="bg-white shadow navbar">
+            <div className="container flex-col sm:flex-row">
+                <div className="flex-1">
+                    <a className="text-xl btn btn-ghost">My Article - Admin</a>
+                </div>
+                <div className="flex-none space-x-4">
+                    {/* input search */}
+                    <div className="join">
+                        <input className="w-full input input-sm sm:w-36 input-bordered join-item" placeholder="pencarian..." />
+                        <button className="rounded-r-lg btn btn-sm join-item">Cari</button>
+                    </div>
+
+                    {/* logout */}
+                    <button className='btn btn-sm btn-error'>
+                        Logout
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import NavbarAdmin from '../components/NavbarAdmin'
 import Footer from '../components/Footer'
 
 export default function AdminLayout({ children }) {
     return (
         <div className='flex flex-col min-h-screen'>
-            <nav>admin navbar</nav>
+            <NavbarAdmin />
 
             <main>
                 {children}
